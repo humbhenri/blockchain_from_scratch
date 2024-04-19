b9e274ae2c91d1d6167cfeb2302202ae7b5ba397
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2024-04-19 14:33:02 -0300

    create block from data posted via http

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,8 @@
 package p2p
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -24,12 +26,36 @@ func handleGetBlocks(w http.ResponseWriter, r *http.Request) {
 	blockchain.GetBlockchain().Print(w)
 }
 
+// handleGetBlocks print the blocks information from the genesis til the end in json format
+func handleCreateBlock(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Method not allowed")
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error reading request body: %v", err)
+		return
+	}
+
+	fmt.Printf("Received POST request with body: %s\n", body)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Received POST request with body: %s", body)
+	blockchain.GetBlockchain().AddBlock(string(body))
+}
+
 // StartServer starts a server to handle incoming p2p requests, it first tries to bind to a fixed port,
 // if that results in an error it tries to bind to next until a port limit is reached.
 // This function blocks.
 func StartServer() {
 	router := mux.NewRouter()
 	router.HandleFunc("/blocks", handleGetBlocks).Methods("GET")
+	router.HandleFunc("/blocks", handleCreateBlock).Methods("POST")
+
 	server := &http.Server{
 		Handler: router,
 	}
--- p2p/p2p_test.go
+++ p2p/p2p_test.go
@@ -1,11 +1,34 @@
 package p2p
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/humbhenri/blockchain_from_scratch/blockchain"
 )
 
-func TestTODO(t *testing.T) {
-	if 1 == 2 {
-		t.Fatal("Impossible")
+func TestCreateBlock(t *testing.T) {
+	blockchain.InitBlockChain()
+
+	req, err := http.NewRequest("POST", "/blocks", strings.NewReader("teste"))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(handleCreateBlock)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Received POST request with body: teste"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
